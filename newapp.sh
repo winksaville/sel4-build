469e2dd4299f71727e837d2af846192bb99b2269
Wink Saville <wink@saville.com>
2015-05-29 19:16:43 -0700

    Change newapp.sh to handle multiple configurations.
    Move the configuration handling into a loop converting all of them
    and also creating a symbolic link in the configs directory instead
    of just linking configs to the new app's master-configs. This allows
    multiple application configurations to exists.

--- newapp.sh
+++ newapp.sh
@@ -22,36 +22,52 @@ fi
 # Copy helloworld to the new apps directory and rename the config
 mkdir apps/${lc}
 cp -r apps/helloworld/* apps/${lc}
-mv apps/${lc}/master-configs/ia32_simulation_helloworld_defconfig apps/${lc}/master-configs/ia32_simulation_${lc}_defconfig
 
 # Change the contents of Kconfig and Kbuild in the new app
 sed -i -e "s/helloworld/${lc}/g" apps/${lc}/K*
-sed -i -e "s/HELLOWORLD/${uc}/g" apps/${lc}/K* apps/${lc}/master-configs/ia32_simulation_${lc}_defconfig
+sed -i -e "s/HELLOWORLD/${uc}/g" apps/${lc}/K*
 sed -i -e "s/Hello World application/${fcuc} application/g" apps/${lc}/Kconfig
 
-# Add the new app to Kconfig .build/ which is symbolicly linked to from the root
+# Add the new app to .build/Kconfig which is symbolicly linked to from the root
 sed -i -e "s/.*seL4 Applications.*/&\n    source \"apps\/${lc}\/Kconfig\"/" .build/Kconfig
 
-# Add the new app to Makefile .build/ which is symbolicly linked to from the root
+# Add the new app to .build/Makefile which is symbolicly linked to from the root
 # Note hack for adding a "\" where the eOl's are, I couldn't make anything else work!
 sed -i -e "s/^.PHONY.*/simulate-${lc}-ia32:\n\tqemu-system-i386 \\\ -eOl-\n\t\t-m 512 -nographic -kernel images\/kernel-ia32-pc99 \\\ -eOl-\n\t\t-initrd images\/${lc}-image-ia32-pc99\n\n&/" .build/Makefile
 sed -i -e "s/ -eOl-//g" .build/Makefile
 
-# Have configs point to the newapp
-rm configs
-ln -s apps/${lc}/master-configs configs
+# Handle the configurations. We need to do three things
+# 1) Rename the files in the destination
+# 2) Change any HELLOWORLD's to ${uc}
+# 3) Add a symbolic link to each of the configurations in the destination
+configurations=(apps/${lc}/master-configs/*)
+for file in "${configurations[@]}"; do
+  # 1) Rename the files in the destination
+  newFile="${file/helloworld/${lc}}"
+  echo "file=$file newFile=$newFile"
+  if [ "$file" != "$newFile" ]; then
+    echo "mv ${file} ${newFile}"
+    mv ${file} ${newFile}
+  fi
 
-# Set the default config and make it and run it
-make ia32_simulation_${lc}_defconfig
-make
+  # 2) Change any HELLOWORLD's to ${uc}
+  sed -i -e "s/HELLOWORLD/${uc}/g" $newFile
+
+  # 3) Add a symbolic link to each of the configurations in the destination
+  newFilename="${newFile##*/}"
+  configsDestFile="configs/$newFilename"
+  rm -f $configsDestFile # delete incase it exists
+  echo "ln -s ../$newFile $configsDestFile"
+  ln -s ../$newFile $configsDestFile
+done
 
 echo ""
 echo "---------------------------------------------------------------"
-echo "Assuming no errors we just configured and made ${lc} app doing:"
+echo "Choose a configuration and then make"
 echo " make ia32_simulation_${lc}_defconfig"
 echo " make"
 echo ""
-echo "To simulate using qemu:"
+echo "Finally to simulate using qemu:"
 echo " make simulate-${lc}-ia32"
 echo ""
 echo "And to end the qemu simulation type:"
